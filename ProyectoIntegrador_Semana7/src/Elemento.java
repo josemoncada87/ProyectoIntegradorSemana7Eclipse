553abe8383002e3a4040e746323b05fbe715b667
Jose Andres Moncada <josemoncada87@hotmail.com>
2016-03-04 11:59:43 -0500

    nueva clase

---
+++ ProyectoIntegrador_Semana7/src/Elemento.java
@@ -0,0 +1,21 @@
+
+public class Elemento {
+	
+	public Elemento() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return super.equals(obj);
+	}
+	
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return super.hashCode();
+	}
+
+}
